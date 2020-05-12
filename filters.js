f690a771080afd452be03870be1360281fffca9f
Peter Cho <pcho51990@gmail.com>
2020-05-12 19:28:15 -0400

    refactored app.js and filters.js

--- filters.js
+++ filters.js
@@ -1,32 +1,113 @@
 import { renderState } from './renders.js';
 
-export const renderFilters = () => {
+export const handleFilters = e => {
   const filterState = localStorage.getItem('filter-state').split(',');
-  const prevState = localStorage.getItem('foodie-state').split(',');
-  const state = localStorage.getItem('foodie-filtered').split(',');
+  const dietParams = localStorage.getItem('diet-params').split(',');
+  const intoleranceParams = localStorage
+    .getItem('intolerance-params')
+    .split(',');
+  let updatedDietParams = [];
+  let updatedIntoleranceParams = [];
+
+  if (e.target.checked) {
+    if (!filterState.includes(e.target.id)) {
+      localStorage.setItem('filter-state', filterState + ',' + e.target.id);
+    }
+    switch (e.target.id) {
+      case 'vegetarian':
+        if (!dietParams.includes('vegetarian')) {
+          dietParams.push('vegetarian');
+          break;
+        }
+      case 'vegan':
+        if (!dietParams.includes('vegan')) {
+          dietParams.push('vegan');
+          break;
+        }
+      case 'glutenFree':
+        if (!intoleranceParams.includes('gluten')) {
+          intoleranceParams.push('gluten');
+          break;
+        }
+      case 'dairyFree':
+        if (!intoleranceParams.includes('dairy')) {
+          intoleranceParams.push('dairy');
+          break;
+        }
+      default:
+        return null;
+    }
+    localStorage.setItem('diet-params', dietParams.join(','));
+    localStorage.setItem('intolerance-params', intoleranceParams.join(','));
+  } else {
+    const nextFilterState = filterState
+      .filter(each => each !== e.target.id)
+      .join(',');
+    localStorage.setItem('filter-state', nextFilterState);
+    switch (e.target.id) {
+      case 'vegetarian':
+        updatedDietParams = dietParams.filter(each => each !== 'vegetarian');
+        break;
+      case 'vegan':
+        updatedDietParams = dietParams.filter(each => each !== 'vegan');
+        break;
+      case 'glutenFree':
+        updatedIntoleranceParams = intoleranceParams.filter(
+          each => each !== 'gluten'
+        );
+        break;
+      case 'dairyFree':
+        updatedIntoleranceParams = intoleranceParams.filter(
+          each => each !== 'dairy'
+        );
+        break;
+      default:
+        return null;
+    }
+    localStorage.setItem('diet-params', updatedDietParams.join(','));
+    localStorage.setItem(
+      'intolerance-params',
+      updatedIntoleranceParams.join(',')
+    );
+  }
+
+  console.log(localStorage.getItem('diet-params'));
+  console.log(localStorage.getItem('intolerance-params'));
+  renderFilters();
+};
+
+export const renderFilters = () => {
+  const filterState = localStorage
+    .getItem('filter-state')
+    .split(',')
+    .filter(each => each !== 'null');
+  const prevState = localStorage
+    .getItem('foodie-state')
+    .split(',')
+    .filter(each => each !== 'null');
+  const state = localStorage
+    .getItem('foodie-filtered')
+    .split(',')
+    .filter(each => each !== 'null');
   let newState = [];
 
   for (let each of filterState) {
-    if (each !== 'null') {
-      if (newState.length === 0) {
-        for (let item of state) {
-          if (item !== 'null') {
-            const data = JSON.parse(localStorage.getItem(item));
-            if (data) {
-              if (data[each]) {
-                if (!newState.includes(item)) {
-                  newState.push(item);
-                }
-              }
+    if (newState.length === 0) {
+      for (let item of state) {
+        const data = JSON.parse(localStorage.getItem(item));
+        if (data) {
+          if (data[each]) {
+            if (!newState.includes(item)) {
+              newState.push(item);
             }
           }
         }
-      } else {
-        for (let item of newState) {
-          const data = JSON.parse(localStorage.getItem(item));
-          if (!data[each]) {
-            newState = newState.filter(recipe => recipe !== item);
-          }
+      }
+    } else {
+      for (let item of newState) {
+        const data = JSON.parse(localStorage.getItem(item));
+        if (!data[each]) {
+          newState = newState.filter(recipe => recipe !== item);
         }
       }
     }
